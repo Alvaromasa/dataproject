a1d27c6e6243e63f96f7c47a660c7993f0ac2af8
Alvaromasa <alvaromasa1@gmail.com>
2022-01-28 19:27:59 +0100

    implementación de varios cambios en el diseño

--- kafka/consumer_kafka_python.py
+++ kafka/consumer_kafka_python.py
@@ -1,9 +1,9 @@
-from kafka import KafkaConsumer
+import kafka
 import json
 
 
 if __name__ == '__main__':
     #Consumidor de Kafka
-    consumer = KafkaConsumer('topic_users', bootstrap_servers='localhost:9092', auto_offset_reset='earliest')
+    consumer = kafka.KafkaConsumer('topic_users', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=True)
     for users in consumer:
         print(json.loads(users.value))
