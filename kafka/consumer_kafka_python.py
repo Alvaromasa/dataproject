37509d6d6bb8fa3af9c95a3396ce0b20b2d4fd60
Álvaro Masa <76207089+Alvaromasa@users.noreply.github.com>
2022-02-02 23:04:20 +0100

    Delete kafka directory

--- kafka/consumer_kafka_python.py
+++
@@ -1,9 +0,0 @@
-import kafka
-import json
-
-
-if __name__ == '__main__':
-    #Consumidor de Kafka
-    consumer = kafka.KafkaConsumer('topic_users', bootstrap_servers=['localhost:9092'], auto_offset_reset='earliest', enable_auto_commit=True)
-    for users in consumer:
-        print(json.loads(users.value))
